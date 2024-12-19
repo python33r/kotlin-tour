0d43ad59c95928f6efed710cdcffa1fbc8c42bed
Nick Efford <nick.efford@gmail.com>
2024-12-19 13:23:45 +0000

    Fixed indentation.

--- examples/classes/init.kts
+++ examples/classes/init.kts
@@ -1,17 +1,17 @@
 // Example of property validation on instantiation
 
 class Person(val name: String) {
-  init {
-    require(!name.isBlank()) { "name cannot be blank or empty" }
-  }
+    init {
+        require(!name.isBlank()) { "name cannot be blank or empty" }
+    }
 
-  var isMarried = false
+    var isMarried = false
 }
 
 fun test(name: String) {
-  println("\nAttempting to create a Person named \"$name\"...")
-  val person = Person(name)
-  println("Person's name is ${person.name}")
+    println("\nAttempting to create a Person named \"$name\"...")
+    val person = Person(name)
+    println("Person's name is ${person.name}")
 }
 
 test("Joe")
