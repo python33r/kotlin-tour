8d48ab1f9ae14cd6b2a3cc8681c6cc5b1fdfb1b0
Nick Efford <nick.efford@gmail.com>
2025-01-09 14:59:56 +0000

    Removed spurious comma from notes script.

--- slides/notes.sh
+++ slides/notes.sh
@@ -2,4 +2,4 @@
 sed -e 's/\/\/show-notes/show-notes/' -e 's/\/\/handout/handout/' slides.typ > tmp.typ
 typst compile tmp.typ
 cpdf -cropbox "841.89pt 0pt 841.89pt 473.563pt" tmp.pdf -o notes.pdf
-rm -f tmp.pdf, tmp.typ
+rm -f tmp.pdf tmp.typ
