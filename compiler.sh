781e90ec9a7fad839d45cdcf35f771618c1c908d
Nick Efford <nick.efford@gmail.com>
2025-01-14 11:59:39 +0000

    Added compiler installation script.

---
+++ compiler.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to download & install Kotlin's command line compiler
+
+if [[ $# == 0 ]]; then
+    echo "Usage: $0 <install-dir>"
+    exit 1
+fi
+
+wget --output-document kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v2.1.0/kotlin-compiler-2.1.0.zip
+unzip -DD kotlinc.zip -d $1
+rm kotlinc.zip
+
+echo
+echo "Remember to modify PATH to include $1/kotlinc/bin"
