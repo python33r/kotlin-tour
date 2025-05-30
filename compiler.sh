a1089c62c19b907be72cbd257515b86ccd3c4a7f
Nick Efford <nick.efford@gmail.com>
2025-05-30 14:07:32 +0100

    Updated compiler install script.

--- compiler.sh
+++ compiler.sh
@@ -6,7 +6,7 @@ if [[ $# == 0 ]]; then
     exit 1
 fi
 
-wget --output-document kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v2.1.10/kotlin-compiler-2.1.10.zip
+curl -L -o kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v2.1.21/kotlin-compiler-2.1.21.zip
 unzip -DD kotlinc.zip -d $1
 rm kotlinc.zip
 
